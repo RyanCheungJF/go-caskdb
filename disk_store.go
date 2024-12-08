8d6962a3d32d5d9199fb4ba7f80de843920067ac
Ryan Cheung <ryan9cheung@gmail.com>
2024-12-08 22:34:48 +0800

    Add ability to read from file

--- disk_store.go
+++ disk_store.go
@@ -56,15 +56,68 @@ type DiskStore struct {
 }
 
 func isFileExists(fileName string) bool {
-	// https://stackoverflow.com/a/12518877
+	// how to check if a file exists in go
+	// https://stackoverflow.com/a/12518877 
 	if _, err := os.Stat(fileName); err == nil || errors.Is(err, fs.ErrExist) {
 		return true
 	}
 	return false
 }
 
+func (diskStore *DiskStore) readFromFile(fileName string) {
+	// just for normal opening, no specific permissions needed to open
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatalf("failed to open file, err : %v", err)
+	}
+	// schedule file to close at the end of the function, 
+	// if not will be closed near after the Open() call
+	defer file.Close()
+
+	// go's loops do not require a cond, as long as we return/ break inside
+	for {
+		// make a bytearray of size 'headerSize'
+		headerByteArray := make([]byte, headerSize)
+		_, err := io.ReadFull(file, headerByteArray)
+		if err == io.EOF {
+			// exit condition
+			break
+		}
+
+		if err != nil {
+			log.Fatalf("failed to read from file while reading header, err := %v", err)
+			break
+		}
+
+		timestamp, keySize, valSize := decodeHeader(headerByteArray)
+		// use internal pointer to read forward
+		key := make([]byte, keySize)
+		_, err = io.ReadFull(file, key)
+		if err != nil {
+			log.Fatalf("failed to read from file while reading key, err := %v", err)
+			break
+		}
+		val := make([]byte, valSize)
+		_, err = io.ReadFull(file, val)
+		if err != nil {
+			log.Fatalf("failed to read from file while reading val, err := %v", err)
+			break
+		}
+
+		totalSize := headerSize + keySize + valSize
+		diskStore.hashmap[string(key)] = NewKeyEntry(timestamp, uint32(diskStore.offset), totalSize)
+		diskStore.offset += int(totalSize)
+	}
+}
+
 func NewDiskStore(fileName string) (*DiskStore, error) {
 	diskStore := &DiskStore{hashmap: make(map[string]KeyEntry)}
+	
+	// if file exists, read from the file's contents and populate hashmap
+	if isFileExists(fileName) {
+		diskStore.readFromFile(fileName)
+	} 
+	
 	// everyone can read write
 	const FILE_PERMISSION = 0666
 	file, err := os.OpenFile(fileName, os.O_APPEND | os.O_RDWR | os.O_CREATE, FILE_PERMISSION)
@@ -80,7 +133,7 @@ func (d *DiskStore) Get(key string) string {
 	// get value from hashmap, in go lookups return a second param which is a bool
 	keyEntry, ok := d.hashmap[key]
 	if !ok {
-		log.Fatalf("key of %v does not exist in diskstore!", key)
+		log.Printf("key of '%v' does not exist in diskstore!", key)
 		return ""
 	}
 
@@ -107,7 +160,17 @@ func (d *DiskStore) Set(key string, value string) {
 }
 
 func (d *DiskStore) Close() bool {
-	panic("implement me")
+	// flush in memory buffer and actually write to disk
+	// uses in memory buffer as writing to disk after every write is expensive
+	if err := d.file.Sync(); err != nil {
+		log.Fatalf("failed to flush in memory buffer to disk, error: %v", err)
+		return false
+	}
+	if err := d.file.Close(); err != nil {
+		log.Fatalf("failed to close file, error: %v", err)
+		return false
+	}
+	return true
 }
 
 func (d *DiskStore) read(position int64, size uint32) []byte {
